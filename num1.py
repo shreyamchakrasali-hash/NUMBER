6f62c914d091e5746b15627b8c92efc32277141e
shreyamchakrasali-hash <shreyamchakrasali@gmail.com>
2025-11-21 10:59:52 +0530

    Fix argument check and update print statements
    Corrected argument check and improved output messages.

--- num1.py
+++ num1.py
@@ -1,25 +1,22 @@
 import sys
-
-# Check if exactly 2 arguments are provided (besides the script name)
-if len(sys.argv) != 3:
+if (sys.argv)!= 3:
     print("Usage: python pythoncal.py <Integer1> <Integer2>")
-else:
     integer1 = int(sys.argv[1])
     integer2 = int(sys.argv[2])
     print(f"Integer1 {integer1} and Integer2 {integer2}")
 
-    # Check integer1
     if integer1 > 0:
         print(f"{integer1} is a positive number")
     elif integer1 < 0:
-        print(f"{integer1} is a negative number")
+        print(f"{integer1} is negative number")
     else:
-        print(f"{integer1} is neither negative nor positive")
-
-    # Check integer2
+        print(f"{integer1} is neither negative or positive")
+    
     if integer2 > 0:
         print(f"{integer2} is a positive number")
     elif integer2 < 0:
-        print(f"{integer2} is a negative number")
+        print(f"{integer2} is negative number")
     else:
         print(f"{integer2} is neither negative nor positive")
+else:
+    print("Invalid input")
